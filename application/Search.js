6f676fba32db13410c6f790d29c214379c4be670
chotiwich <chotiwich6793@gmail.com>
2020-05-21 04:22:53 +0700

    แก้ JS SEARCH

--- application/Search.js
+++ application/Search.js
@@ -24,7 +24,7 @@ $.getJSON(urlAPI).done(function (data) {
   $.each(data, function (k, item) {
     // console.log(item);
     line += "<tr>";
-    line += "<td > <a href = http://localhost/hermes/page/checkin_edit.php?id="+item.bl_id+" ><button type= 'button' class= ' btn-round btn-fill btn-info' id='load_checkin'>Info</button></a> </td>";
+    line += "<td > <a href = http://localhost/hermes/page/checkin_edit.php?id="+item.bl_id+" ><button type= 'button' class= '  btn btn-info btn-sm btn-round ' id='load_checkin'>Info</button></a> </td>";
     line += "<td >" + item.ginfo_first_name + "</td>";
     line += "<td >" + item.room_name + "</td>";
     line += "<td >" + item.ginfo_first_name + "</td>";
@@ -38,9 +38,8 @@ $.getJSON(urlAPI).done(function (data) {
   $("#tb1").append(line);
 
   $('#datatable1').DataTable({
-    "pageLength": 25,
-    "autoWidth": true,
-    "searching": true
+    "pageLength": 50,
+    "autoWidth": true
   });
 });
 
